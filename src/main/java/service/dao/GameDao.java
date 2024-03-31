c0d8ff16720fc4eeeb592bba590a215e07c9ab18
이상진 <priceelees@gmail.com>
2024-04-01 04:02:01 +0900

    feat(GameDao): 현재 게임에서 이동 가능한 색상을 game 테이블에 저장하고, 불러오는 클래스 생성

---
+++ src/main/java/service/dao/GameDao.java
@@ -0,0 +1,70 @@
+package service.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class GameDao {
+
+    private static final GameDao instance = new GameDao();
+    private static final ChessDBUtil chessDBUtil = ChessDBUtil.getInstance();
+
+    private GameDao() {
+    }
+
+    public static GameDao getInstance() {
+        return instance;
+    }
+
+    public void createColor() {
+        final String query = "INSERT INTO game VALUES(?, ?)";
+        try (final Connection connection = chessDBUtil.getConnection()) {
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setInt(1, 1);
+            preparedStatement.setString(2, "WHITE");
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void saveColor(String movingColor) {
+        final String query = "UPDATE game SET moving_color = ? WHERE game_id = 1";
+        try (final Connection connection = chessDBUtil.getConnection()) {
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setString(1, movingColor);
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public String loadColor() {
+        final String query = "SELECT moving_color FROM game WHERE game_id = 1";
+        try (final Connection connection = chessDBUtil.getConnection()) {
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            return getColor(resultSet);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private String getColor(ResultSet resultSet) throws SQLException {
+        if (resultSet.next()) {
+            return resultSet.getString("moving_color");
+        }
+        throw new IllegalStateException("게임이 존재하지 않습니다");
+    }
+
+    public void delete() {
+        final String query = "DELETE FROM game WHERE game_id = 1";
+        try (final Connection connection = chessDBUtil.getConnection()) {
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            throw new IllegalStateException("게임이 존재하지 않습니다.");
+        }
+    }
+}
