8d32c2a5b48f68c1e45ad203327a4d686e7b2877
이상진 <priceelees@gmail.com>
2024-04-01 03:58:11 +0900

    feat(ChessPieceMapper): 체스 말의 타입과 색상을 이용하여 객체로 매핑하는 클래스 생성

---
+++ src/main/java/service/mapper/ChessPieceMapper.java
@@ -0,0 +1,59 @@
+package service.mapper;
+
+import domain.piece.Bishop;
+import domain.piece.BlackPawn;
+import domain.piece.Blank;
+import domain.piece.Color;
+import domain.piece.King;
+import domain.piece.Knight;
+import domain.piece.Queen;
+import domain.piece.Rook;
+import domain.piece.WhitePawn;
+import domain.piece.base.ChessPiece;
+import domain.piece.type.PieceType;
+import java.util.Arrays;
+
+public enum ChessPieceMapper {
+
+    WHITE_PAWN(PieceType.PAWN, Color.WHITE, new WhitePawn()),
+    BLACK_PAWN(PieceType.PAWN, Color.BLACK, new BlackPawn()),
+
+    WHITE_KING(PieceType.KING, Color.WHITE, new King(Color.WHITE)),
+    BLACK_KING(PieceType.KING, Color.BLACK, new King(Color.BLACK)),
+
+    WHITE_BISHOP(PieceType.BISHOP, Color.WHITE, new Bishop(Color.WHITE)),
+    BLACK_BISHOP(PieceType.BISHOP, Color.BLACK, new Bishop(Color.BLACK)),
+
+    WHITE_QUEEN(PieceType.QUEEN, Color.WHITE, new Queen(Color.WHITE)),
+    BLACK_QUEEN(PieceType.QUEEN, Color.BLACK, new Queen(Color.BLACK)),
+
+    WHITE_KNIGHT(PieceType.KNIGHT, Color.WHITE, new Knight(Color.WHITE)),
+    BLACK_KNIGHT(PieceType.KNIGHT, Color.BLACK, new Knight(Color.BLACK)),
+
+    WHITE_ROOK(PieceType.ROOK, Color.WHITE, new Rook(Color.WHITE)),
+    BLACK_ROOK(PieceType.ROOK, Color.BLACK, new Rook(Color.BLACK)),
+
+    BLANK(PieceType.BLANK, Color.ANY, Blank.getInstance());
+
+    private final PieceType pieceType;
+    private final Color color;
+    private final ChessPiece instance;
+
+    ChessPieceMapper(PieceType pieceType, Color color, ChessPiece instance) {
+        this.pieceType = pieceType;
+        this.color = color;
+        this.instance = instance;
+    }
+
+    public static ChessPiece getInstance(PieceType pieceType, Color color) {
+        return Arrays.stream(values())
+                .filter(piece -> piece.isSamePiece(pieceType, color))
+                .findAny()
+                .orElseThrow(() -> new IllegalArgumentException("해당되는 말이 존재하지 않습니다."))
+                .instance;
+    }
+
+    private boolean isSamePiece(PieceType pieceType, Color color) {
+        return this.pieceType == pieceType && this.color == color;
+    }
+}
