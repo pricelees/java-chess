c8cbd8400a1e8a950da7639336324a9056317970
이상진 <priceelees@gmail.com>
2024-04-01 03:49:53 +0900

    refactor(Command): load, save 명령어 추가 및 기능 분리
    - getInitialCommand 는 START 또는 LOAD 만을 반환한다.
    - getPlayingCommand 는 이외의 명령어를 반환한다.

--- src/main/java/view/util/Command.java
+++ src/main/java/view/util/Command.java
@@ -5,8 +5,10 @@ import java.util.Arrays;
 public enum Command {
 
     START("start"),
+    LOAD("load"),
     MOVE("move"),
     STATUS("status"),
+    SAVE("save"),
     END("end");
 
     private final String identifier;
@@ -20,5 +22,21 @@ public enum Command {
                 .filter(command -> command.identifier.equals(identifier))
                 .findAny()
                 .orElseThrow(() -> new IllegalArgumentException("해당되는 식별자가 존재하지 않습니다."));
+    }
+
+    public static Command getInitialCommand(String identifier) {
+        Command command = from(identifier);
+        if (command != START && command != LOAD) {
+            throw new IllegalArgumentException("게임을 먼저 시작하거나 불러와야 합니다.");
+        }
+        return command;
+    }
+
+    public static Command getPlayingCommand(String identifier) {
+        Command command = from(identifier);
+        if (command == START || command == LOAD) {
+            throw new IllegalArgumentException("게임이 이미 시작되었습니다.");
+        }
+        return command;
     }
 }
