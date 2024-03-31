88b19af84d6b24cfbe15de45ada760a79c7b3f0d
이상진 <priceelees@gmail.com>
2024-04-01 04:10:46 +0900

    refactor(OutputView): 기능 추가 및 수정
    - 추가된 게임 명령어 반영
    - 게임 정보를 출력할 때 색상까지 출력하도록 수정

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -1,22 +1,30 @@
 package view;
 
-import controller.dto.ChessBoardStatusDTO;
+import controller.dto.ChessBoardStatusDto;
+import controller.dto.ScoreDto;
 import java.util.List;
 
 public class OutputView {
 
-    private static final String SCORE_FORMAT = "내 점수: %.1f, 상대 점수: %.1f.";
+    private static final String SCORE_FORMAT = "%s 점수: %.1f, %s 점수: %.1f.";
 
     public void printGameGuide() {
         System.out.print("""
                 > 체스 게임을 시작합니다.
                 > 게임 시작 : start
+                > 이전 게임 불러오기 : load
+                > 현재 점수 확인 : status
+                > 게임 저장 후 종료 : save
                 > 게임 종료 : end
                 > 게임 이동 : move source위치 target위치 - 예. move b2 b3
                 """);
     }
 
-    public void printBoard(ChessBoardStatusDTO chessBoardStatusDTO) {
+    public void printCurrentTurnColor(String colorName) {
+        System.out.println(colorName + "이(가) 이동할 차례입니다.");
+    }
+
+    public void printBoard(ChessBoardStatusDto chessBoardStatusDTO) {
         chessBoardStatusDTO.boardStatus().forEach(this::printRow);
     }
 
@@ -25,17 +33,17 @@ public class OutputView {
         System.out.println();
     }
 
-    public void printScore(double playerScore, double opponentScore) {
-        System.out.println(formatScore(playerScore, opponentScore));
+    public void printScore(ScoreDto player, ScoreDto opponent) {
+        System.out.println(formatScore(player, opponent));
     }
 
-    private String formatScore(double playerScore, double opponentScore) {
-        return SCORE_FORMAT.formatted(playerScore, opponentScore);
+    private String formatScore(ScoreDto player, ScoreDto opponent) {
+        return SCORE_FORMAT.formatted(player.colorName(), player.score(), opponent.colorName(), opponent.score());
     }
 
-    public void printScoreResult(double playerScore, double opponentScore) {
+    public void printScoreResult(ScoreDto player, ScoreDto opponent) {
         System.out.println(
-                formatScore(playerScore, opponentScore) + " " + formatResult(playerScore, opponentScore)
+                formatScore(player, opponent) + " " + formatResult(player.score(), opponent.score())
         );
     }
 
@@ -49,7 +57,7 @@ public class OutputView {
         return "비겼습니다.";
     }
 
-    public void printMessageWhenRemoveOpponentKing() {
-        System.out.println("상대의 King이 제거되었습니다. 승리를 축하합니다!");
+    public void printMessageWhenRemoveOpponentKing(String removedKingColor, String winner) {
+        System.out.println(removedKingColor + "의 KING이 제거되었습니다. " + winner + " 승리!");
     }
 }
