022e3c9756e0d6630aea812d5a79efcf71dcee50
이상진 <priceelees@gmail.com>
2024-04-01 03:55:56 +0900

    feat(CoordinateMapper): 좌표와, 해당 좌표의 이름을 캐싱한 뒤, 매핑하는 별도의 클래스 생성
    - "a1"을 입력하면 new Coordinate(7, 0)을 반환
    - new Coordinate(7, 0)을 입력하면 "a1"을 반환.

---
+++ src/main/java/service/mapper/CoordinateMapper.java
@@ -0,0 +1,34 @@
+package service.mapper;
+
+import domain.coordinate.Column;
+import domain.coordinate.Coordinate;
+import domain.coordinate.Row;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class CoordinateMapper {
+
+    public static final Map<String, Coordinate> CACHE_FROM_NAME = new HashMap<>();
+    public static final Map<Coordinate, String> CACHE_FROM_COORDINATE = new HashMap<>();
+
+    static {
+        Arrays.stream(Column.values())
+                .forEach(column -> Arrays.stream(Row.values()).forEach(row -> {
+                    String name = column.getLowerName() + row.getName();
+                    CACHE_FROM_NAME.put(name, new Coordinate(row.getValue(), column.getValue()));
+                    CACHE_FROM_COORDINATE.put(new Coordinate(row.getValue(), column.getValue()), name);
+                }));
+    }
+
+    private CoordinateMapper() {
+    }
+
+    public static Coordinate getCoordinate(String name) {
+        return CACHE_FROM_NAME.get(name);
+    }
+
+    public static String getCoordinateName(Coordinate coordinate) {
+        return CACHE_FROM_COORDINATE.get(coordinate);
+    }
+}
